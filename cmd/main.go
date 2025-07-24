073dbb1edc305e4c912f732c0338357505771f7f
LENDRA SYAPUTRA <l3noneone@gmail.com>
2025-07-24 16:03:55 +0700

    update dockerfile

--- cmd/main.go
+++ cmd/main.go
@@ -1,35 +1,39 @@
 package main
 
 import (
+	configs "bella/config"
+	"bella/db"
+	"bella/internal/notifier"
+	"bella/setup"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
-	"bella/config"
-	"bella/db"
-	"bella/internal/notifier"
-	"bella/setup"
 
 	"github.com/robfig/cron/v3"
 )
 
 func main() {
-	log.Println("Memulai Bella Alert System ...")
+	log.Println("Memulai Bella Alert System ....")
 
 	config := configs.LoadConfig()
+
 	allConnections := db.InitializeDatabases(config)
+
 	telegramNotifier := notifier.NewTelegramNotifier(config.TelegramToken, config.TelegramChatID)
+
 	scheduler := cron.New()
 
 	setup.RegisterServicesAndTasks(allConnections, telegramNotifier, scheduler, config)
-	
+
 	if len(scheduler.Entries()) > 0 {
 		scheduler.Start()
-		log.Printf("Scheduler berjalan dengan %d tugas.", len(scheduler.Entries()))
+		log.Printf("Scheduler berhasil dimulai dengan %d tugas.", len(scheduler.Entries()))
 	} else {
-		log.Println("Tidak ada koneksi database yang aktif. Tidak ada tugas yang dijadwalkan.")
+		log.Println("Tidak ada tugas yang berhasil didaftarkan di scheduler. Aplikasi akan berhenti karena tidak ada pekerjaan yang harus dilakukan.")
+		return
 	}
-	
+
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 	<-quit
